2efa70b7a643a549559da4a972e97255e67a6ade
Miek Gieben <miek@science.ru.nl>
2022-11-22 13:03:34 +0100

    server: redo metrics
    Current way of doing metrics generates a new one for each state change
    
    gitopper_service_info{state="OK", hash="...", service=".."}
    gitopper_service_info{state="FREEZE", hash="...", service=".."}
    
    etc. What we want is 1 metric that tells us the state:
    
    gitopper_service_hash{service="..."} <hash>
    
    where hash is the SHA1 converted to a float64
    
    State like wise needs to be encoded as a float as well, so we just use
    the value we created internally.
    
    gitopper_service_state{service="..."} <state>
    
    where state is 0 1 2 3 4, etc. depending on the number of states.
    
    Then maybe lastly we want a state change timestamp:
    
    gitopper_service_change_timestamp{service=".."} <epoch> when we the last state change.
    
    Signed-off-by: Miek Gieben <miek@science.ru.nl>

--- metrics.go
+++ metrics.go
@@ -7,9 +7,25 @@ import (
 
 // do we have a latecy that we can track?
 
-var metricServiceHash = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Namespace: "gitopper",
-	Subsystem: "service",
-	Name:      "info",
-	Help:      "Current hash and state for this service",
-}, []string{"service", "hash", "state"})
+var (
+	metricServiceHash = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "gitopper",
+		Subsystem: "service",
+		Name:      "hash",
+		Help:      "Current hash for this service.",
+	}, []string{"service"})
+
+	metricServiceState = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "gitopper",
+		Subsystem: "service",
+		Name:      "state",
+		Help:      "Current state for  this service.",
+	}, []string{"service"})
+
+	metricServiceTimestamp = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "gitopper",
+		Subsystem: "service",
+		Name:      "change_timestamp",
+		Help:      "Timestamp for last service change.",
+	}, []string{"service"})
+)
